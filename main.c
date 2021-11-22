046bf2c56479b766658aa318edcc043917e483fe
cinthya-g <cinthya.lorena.gonzalez@hotmail.com>
2021-11-21 19:57:51 -0600

    Tablero gráfico y fichas

--- main.c
+++ main.c
@@ -6,33 +6,60 @@
 #include "parchis.h"
 #include "turnos.h"
 
-
-int main()
+int main(void)
 {
-    /*
-     * //Pruebas con raylib, IGNORAR!!
-    const int screenWidth = 1000;
-    const int screenHeight = 800;
-    InitWindow(screenWidth, screenHeight, "Tablero");
-    SetTargetFPS(60);
-    while(!WindowShouldClose())
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int width = 1600;
+    const int height = 900;
+
+    InitWindow(width, height, "PARCHIS - KER Y TIA");
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    //Iniciar tablero y fichas
+    Image fondo = LoadImage("..\\tablero.png");
+    Image imgR1 = LoadImage("..\\fichas\\R1.png");
+    Texture2D textureBg = LoadTextureFromImage(fondo);
+    Texture2D textureficha1 = LoadTextureFromImage(imgR1);
+    UnloadImage(fondo);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(LIGHTGRAY);
-            DrawText("Parchis", 450, 20, 25, DARKBLUE);
-            DrawRectangle(40, 60, 920, 700, SKYBLUE);
 
+        ClearBackground(RAYWHITE);
 
-            EndDrawing();
+        DrawTexture(textureBg, 0, 0, WHITE);
+        DrawTexture(textureficha1, 405, 410, WHITE);
+
+        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
-    CloseWindow();
-     */
 
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+
+/*
     Board *juego = newBoard();
     creacionMaestra(juego);
     displayBoard(juego);
     Turno();
-
+    char player=quienInicia();
+    printf("%c",player);
+*/
 
     return 0;
 }
